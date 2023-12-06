cedec25b09b7ad55d1a8f62ff9055b373d32624a
Silveira Vinicius (CaP/ETS) <ivs6ca@bosch.com>
2023-12-06 11:00:07 -0300

    Requisições novas de testes
    Testando requisições para o funcionamento

--- Backend/Files/crud.py
+++ Backend/Files/crud.py
@@ -2,27 +2,29 @@ import Files.models as models
 import Files.schemas as sc
 from uuid import uuid4
 from sqlalchemy.orm import Session
+from sqlalchemy import or_
+import bcrypt
 
-# async def create_types_entertainment(db: Session, objs: sc.TypeBase):
-#     if 
-#     db_item = models.Type(**sc.TypeBase)
-#     db_item.id = uuid4()
-#     db_item.type = 'Movie'
+## User
+# GET
+async def get_user(db: Session, user: sc.UserBase):
+    return db.query(models.User).filter(or_(models.User.email == user.email, models.User.username == user.username)).first()
 
-# @app.post('/users/', status_code=status.HTTP_201_CREATED)
-# async def create_user(user: UserBase, db: db_dependency):
-#     db_user = models.User(**user.model_dump())
-#     db_user.id = uuid4()
-#     db.add(db_user)
-#     db.commit()
-    
-# async def get_user(user_id : str, db: db_dependency):
-#     user = db.query(models.User).filter(models.User.id == user_id).first()
-#     if user is None:
-#         raise HTTPException(status_code=404, detail='User not found')
-#     return user
+# POST 
+async def create_user(db: Session, user: sc.UserBase):
+    db_user = models.User(**user.model_dump())
+    db_user.id = uuid4()
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+## Entertainment
+
+# GET
 
 
+# POST
 async def create_entertainment(db: Session, entertainment: sc.EntertainmentBase):
     db_entertainment = models.Entertainment(**entertainment.model_dump())
     db_entertainment.id = uuid4()
@@ -30,3 +32,16 @@ async def create_entertainment(db: Session, entertainment: sc.EntertainmentBase)
     db.commit()
     db.refresh(db_entertainment)
     return db_entertainment    
+
+# PUT
+
+
+# DELETE
+
+## Type
+
+# GET
+async def get_types(db: Session):
+    return db.query(models.Type).all()
+
+
--- Backend/main.py
+++ Backend/main.py
@@ -19,7 +19,23 @@ def get_db():
         
 db_dependency = Annotated[Session, Depends(get_db)]
 
-@app.post('/Create/Entertainment')
+
+## Requisições GET - Types
+@app.get('/Get/Types')
+async def get_types(db: db_dependency):
+    return await crud.get_types(db=db)
+
+
+## Requisições POST - Entertainment
+@app.post('/Post/Entertainment')
 async def create_entertainment(db: db_dependency, entertainment: sc.EntertainmentBase):
-    return await crud.create_entertainment(db=db, entertainment=entertainment)
-    
+    return await crud.create_entertainment(db=db, entertainment=entertainment)    
+
+## Requisições POST - User
+@app.post('/Post/User')
+async def create_user(db: db_dependency, user: sc.UserBase):
+    db_user = await crud.get_user(db=db, user=user)
+    print(db_user)
+    if db_user:
+        raise HTTPException(status_code=400, detail='User already exists')
+    return await crud.create_user(db=db, user=user)
